a95ad8de1763ab09c821f80925b41470472856f9
fatimazaarate <fatima.zaarate@gmail.com>
2024-08-06 09:42:10 +0100

    react config

---
+++ food-delivery-app/src/App.jsx
@@ -0,0 +1,15 @@
+// install extension es7 react...
+// rafce to write the model of app.jsx
+
+import React from "react";
+import Navbar from "./components/Navbar/navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
---
+++ food-delivery-app/src/components/Navbar/navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./navbar.css";
+
+const navbar = () => {
+  return <div>this is navbar</div>;
+};
+
+export default navbar;
