3ba02c8394042f03d5c6eaf9e0c21f9c1f408bea
fatimazaarate <fatima.zaarate@gmail.com>
2024-08-09 12:15:46 +0100

    changing folder name to front end

---
+++ front-end/src/App.jsx
@@ -0,0 +1,24 @@
+// install extension es7 react...
+// rafce to write the model of app.jsx
+
+import React from "react";
+import Navbar from "./components/Navbar/navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/home";
+import Cart from "./pages/Cart/cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/Order" element={<PlaceOrder />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ front-end/src/pages/Home/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.css";
+import Header from "../../components/Header/Header";
+
+const home = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default home;
