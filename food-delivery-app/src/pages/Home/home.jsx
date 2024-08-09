c0d2c686792eabcbacdf5fb57e562e86e8e22e42
fatimazaarate <fatima.zaarate@gmail.com>
2024-08-09 11:50:59 +0100

    website header done

--- food-delivery-app/src/pages/Home/home.jsx
+++ food-delivery-app/src/pages/Home/home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./home.css";
+import Header from "../../components/Header/Header";
 
 const home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default home;
